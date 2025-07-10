5fef7da20371c1f044c3a18c2fd7a1b6bb4966c5
Ned <ned.l@itcgroup.io>
2025-07-10 18:01:53 +0700

    added highlighted dates

--- app.vue
+++ app.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import CalendarPicker from './components/CalendarPicker/CalendarPicker.vue';
-
 </script>
 
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
     <CalendarPicker :bookedDates="{loading: true}"/>
   </div>
 </template>
--- components/CalendarPicker/CalendarPicker.vue
+++ components/CalendarPicker/CalendarPicker.vue
@@ -1,67 +1,108 @@
 <script setup>
-    import dayjs from 'dayjs';
-    import LeftArrow from '../Icons/LeftArrow.vue';
-    import RightArrow from '../Icons/RightArrow.vue';
-    const bookedDates = defineProps({});
-    const currentPage = ref(0);
-    const startDate = ref('');
-    const endDate = ref('');
-    const months = computed(() => {
-        if (bookedDates?.loading)
-            return;
-        
-        const months = new Array(24);
-        const today = dayjs();
+  import dayjs from 'dayjs';
+  import LeftArrow from '../Icons/LeftArrow.vue';
+  import RightArrow from '../Icons/RightArrow.vue';
 
-        for (let i = 0; i < 24; i++) {
-            const firstDay = today.add(i, 'month').startOf('month');
-            const totalDays = firstDay.daysInMonth();
-            const blankDatesStart = new Array(firstDay.day());
+  const bookedDates = defineProps({});
+  const currentPage = ref(0);
+  const startDate = ref('');
+  const endDate = ref('');
+  const months = computed(() => {
+    if (bookedDates?.loading)
+      return;
+    
+    const months = new Array(24);
+    const today = dayjs();
 
-            const days = Array.from({ length: totalDays }, (_, day) =>
-                firstDay.add(day, 'day')
-            );
+    for (let i = 0; i < 24; i++) {
+      const firstDay = today.add(i, 'month').startOf('month');
+      const totalDays = firstDay.daysInMonth();
+      const blankDatesStart = new Array(firstDay.day());
 
-            months[i] = {
-                year: firstDay.year(),
-                month: firstDay.month() + 1, // dayjs month is 0-based
-                label: firstDay.format('MMMM YYYY'),
-                blankDatesStart,
-                days
-            };
-        }
+      const days = Array.from({ length: totalDays }, (_, day) =>
+        firstDay.add(day, 'day')
+      );
 
-        return months;
-    });
-
-    const toPrevPage = () => {
-        currentPage.value > 0 && currentPage.value--;
+      months[i] = {
+        year: firstDay.year(),
+        month: firstDay.month() + 1, // dayjs month is 0-based
+        label: firstDay.format('MMMM YYYY'),
+        blankDatesStart,
+        days
+      };
     }
 
-    const toNextPage = () => {
-        currentPage.value < 23 && currentPage.value++;
-    }
+    return months;
+  });
 
-    const selectDate = (e) => {
-        if(!startDate.value)
-            startDate = e.target.textContent;
-        else if (!endDate.value)
-            endDate.value = e.target.textContent;
+  function formatDate(day) {
+    return day.format('YYYY-MM-DD')
+  };
+
+  function inSelectedRange(day) {
+    if (!startDate.value || !endDate.value) return '';
+
+    const start = dayjs(startDate.value);
+    const end = dayjs(endDate.value);
+    const current = dayjs(day);
+
+    return current.isAfter(start) && current.isBefore(end) ? 'bg-red-200' : '';
+  };
+
+  function isSelected(day){
+    const dateStr = formatDate(day);
+    return dateStr === startDate.value || dateStr === endDate.value ? 'bg-red-400' : '';
+  };
+
+  function toPrevPage() {
+    currentPage.value > 0 && currentPage.value--;
+  }
+
+  function toNextPage(){
+    currentPage.value < 23 && currentPage.value++;
+  }
+
+  function selectDate(e) {
+    const button = e.target.closest('button[data-date]');
+    if (!button) return;
+
+    const selectedDate = button.getAttribute('data-date');
+
+    if (!startDate.value) 
+      startDate.value = selectedDate;
+    else {
+      const startDateObj = new Date(startDate.value);
+      const endDateObj = new Date(selectedDate);
+
+      if (startDateObj < endDateObj)
+        endDate.value = selectedDate;
     }
+  }
+
+  function clearDate() {
+    startDate.value = '';
+    endDate.value = '';
+  }
 </script>
 <template>
-  <form class="flex flex-row gap-5">
-    <input type="text" class="hidden" v-model="startDate">
-    <input type="text" class="hidden" v-model="endDate">
+  <form class="min-h-[380px] flex flex-col justify-between">
+    <input type="text" name="startDate" class="hidden" :value="startDate">
+    <input type="text" name="endDate" class="hidden" :value="endDate">
 
+    <div class="flex flex-row gap-5">
     <!-- Left Calendar -->
-    <div id="left-calendar" class="w-[400px]">
+      <div id="left-calendar" class="w-[400px]">
       <!-- Header -->
-      <div class="flex justify-between items-center mb-2">
-        <button v-if="currentPage > 0" type="button" @click="toPrevPage">
-            <LeftArrow/>
+      <div class="relative flex items-center justify-center mb-2">
+        <button
+          v-show="currentPage > 0"
+          type="button"
+          @click="toPrevPage"
+          class="absolute left-0"
+        >
+          <LeftArrow />
         </button>
-        <p class="text-lg font-semibold">{{ months[currentPage].label }}</p>
+        <p class="text-lg font-semibold text-center">{{ months[currentPage].label }}</p>
       </div>
 
       <!-- Week Days -->
@@ -81,21 +122,32 @@
         <button
           v-for="day in months[currentPage].days"
           :key="day.toISOString()"
+          :data-date="formatDate(day)"
+          :class="[
+            isSelected(day),
+            inSelectedRange(day),
+            'w-10 h-10 flex items-center justify-center rounded-full border-2 border-white hover:border-red-500 transition duration-200 cursor-pointer'
+          ]"          
           type="button"
-          class="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 hover:border-blue-500 transition duration-200"
+          class="w-10 h-10 flex items-center justify-center rounded-full border-2 border-white hover:border-red-500 transition duration-200 cursor-pointer"
         >
           {{ day.date() }}
         </button>
       </div>
-    </div>
+      </div>
 
     <!-- Right Calendar -->
-    <div id="right-calendar" class="w-[400px]">
+      <div id="right-calendar" class="w-[400px]">
       <!-- Header -->
-      <div class="flex justify-between items-center mb-2">
-        <p class="text-lg font-semibold">{{ months[currentPage + 1].label }}</p>
-        <button v-if="currentPage < 23" type="button" @click="toNextPage">
-            <RightArrow/>
+      <div class="relative flex items-center justify-center mb-2">
+        <p class="text-lg font-semibold text-center">{{ months[currentPage + 1].label }}</p>
+        <button
+          v-show="currentPage < 23"
+          type="button"
+          @click="toNextPage"
+          class="absolute right-0"
+        >
+          <RightArrow />
         </button>
       </div>
 
@@ -116,13 +168,23 @@
         <button
           v-for="day in months[currentPage + 1].days"
           :key="day.toISOString()"
+          :data-date="formatDate(day)"
+          :class="[
+            isSelected(day),
+            inSelectedRange(day),
+            'w-10 h-10 flex items-center justify-center rounded-full border-2 border-white hover:border-red-500 transition duration-200 cursor-pointer'
+          ]"
           type="button"
-          class="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 hover:border-blue-500 transition duration-200"
+          class="w-10 h-10 flex items-center justify-center rounded-full border-2 border-white hover:border-red-500 transition duration-200 cursor-pointer"
         >
           {{ day.date() }}
         </button>
       </div>
+      </div>
     </div>
+    <button @click="clearDate" class="border-2 rounded-sm bg-red-400 text-white cursor-pointer w-16 p-2" type="button">
+      Clear
+    </button>
   </form>
 </template>
 
