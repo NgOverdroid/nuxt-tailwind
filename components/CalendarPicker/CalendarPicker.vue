e6a93de3f86c011708775398d0266d65c6cc4068
Ned <ned.l@itcgroup.io>
2025-07-10 14:11:52 +0700

    added more icons and calendar

--- components/CalendarPicker/CalendarPicker.vue
+++ components/CalendarPicker/CalendarPicker.vue
@@ -1,5 +1,7 @@
 <script setup>
     import dayjs from 'dayjs';
+    import LeftArrow from '../Icons/LeftArrow.vue';
+    import RightArrow from '../Icons/RightArrow.vue';
     const bookedDates = defineProps({});
     const currentPage = ref(0);
     const startDate = ref('');
@@ -15,7 +17,6 @@
             const firstDay = today.add(i, 'month').startOf('month');
             const totalDays = firstDay.daysInMonth();
             const blankDatesStart = new Array(firstDay.day());
-            const blankDatesEnd = new Array(35 - (totalDays + blankDatesStart.length));
 
             const days = Array.from({ length: totalDays }, (_, day) =>
                 firstDay.add(day, 'day')
@@ -26,7 +27,6 @@
                 month: firstDay.month() + 1, // dayjs month is 0-based
                 label: firstDay.format('MMMM YYYY'),
                 blankDatesStart,
-                blankDatesEnd,
                 days
             };
         }
@@ -50,58 +50,80 @@
     }
 </script>
 <template>
-    <form class="flex flex-row">
-        <input type="text" class="hidden" v-model="startDate">
-        <input type="text" class="hidden" v-model="endDate">
-        <div id="left-calendar">
-            <!--Navigation bar-->
-            <div>
-                <button type="button" @click="toPrevPage"></button>
-                <p>{{ `${months[currentPage].month} ${months[currentPage].year}` }}</p>
-            </div>
-            <!--Days in a week-->
-            <div class="flex flex-row no-wrap">
-                <div>SUN</div>
-                <div>MON</div>
-                <div>TUE</div>
-                <div>WED</div>
-                <div>THU</div>
-                <div>FRI</div>
-                <div>SAT</div>
-            </div>
-            <!--Days in a month-->
-            <div @click="selectDate($event)">
-                <div v-for="_ in months[currentPage].blankDatesStart"></div>
-                <div v-for="day in months[currentPage].days" :key="day.toISOString()">
-                    {{ day.getDate() }}
-                </div> 
-                <div v-for="_ in months[currentPage].blankDatesEnd"></div>           
-            </div>
-        </div>
-        <div id="right-calendar">
-            <!--Navigation bar-->
-            <div>
-                <p>{{ `${months[currentPage + 1].month} ${months[currentPage + 1].year}` }}</p>
-                <button type="button" @click="toNextPage"></button>
-            </div>
-            <!--Days in a week-->
-            <div class="flex flex-row no-wrap">
-                <div>SUN</div>
-                <div>MON</div>
-                <div>TUE</div>
-                <div>WED</div>
-                <div>THU</div>
-                <div>FRI</div>
-                <div>SAT</div>
-            </div>
-            <!--Days in a month-->
-            <div @click="selectDate($event)">
-                <div v-for="_ in months[currentPage + 1].blankDatesStart"></div>
-                <div v-for="day in months[currentPage + 1].days" :key="day.toISOString()">
-                    {{ day.getDate() }}
-                </div> 
-                <div v-for="_ in months[currentPage + 1].blankDatesEnd"></div>           
-            </div>
-        </div>
-    </form>
+  <form class="flex flex-row gap-5">
+    <input type="text" class="hidden" v-model="startDate">
+    <input type="text" class="hidden" v-model="endDate">
+
+    <!-- Left Calendar -->
+    <div id="left-calendar" class="w-[400px]">
+      <!-- Header -->
+      <div class="flex justify-between items-center mb-2">
+        <button v-if="currentPage > 0" type="button" @click="toPrevPage">
+            <LeftArrow/>
+        </button>
+        <p class="text-lg font-semibold">{{ months[currentPage].label }}</p>
+      </div>
+
+      <!-- Week Days -->
+      <div class="grid grid-cols-7 text-center font-bold mb-1">
+        <div>SUN</div>
+        <div>MON</div>
+        <div>TUE</div>
+        <div>WED</div>
+        <div>THU</div>
+        <div>FRI</div>
+        <div>SAT</div>
+      </div>
+
+      <!-- Days Grid -->
+      <div class="grid grid-cols-7 gap-1" @click="selectDate($event)">
+        <div v-for="_ in months[currentPage].blankDatesStart" class="w-10 h-10"></div>
+        <button
+          v-for="day in months[currentPage].days"
+          :key="day.toISOString()"
+          type="button"
+          class="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 hover:border-blue-500 transition duration-200"
+        >
+          {{ day.date() }}
+        </button>
+      </div>
+    </div>
+
+    <!-- Right Calendar -->
+    <div id="right-calendar" class="w-[400px]">
+      <!-- Header -->
+      <div class="flex justify-between items-center mb-2">
+        <p class="text-lg font-semibold">{{ months[currentPage + 1].label }}</p>
+        <button v-if="currentPage < 23" type="button" @click="toNextPage">
+            <RightArrow/>
+        </button>
+      </div>
+
+      <!-- Week Days -->
+      <div class="grid grid-cols-7 text-center font-bold mb-1">
+        <div>SUN</div>
+        <div>MON</div>
+        <div>TUE</div>
+        <div>WED</div>
+        <div>THU</div>
+        <div>FRI</div>
+        <div>SAT</div>
+      </div>
+
+      <!-- Days Grid -->
+      <div class="grid grid-cols-7 gap-1" @click="selectDate($event)">
+        <div v-for="_ in months[currentPage + 1].blankDatesStart" class="w-10 h-10"></div>
+        <button
+          v-for="day in months[currentPage + 1].days"
+          :key="day.toISOString()"
+          type="button"
+          class="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 hover:border-blue-500 transition duration-200"
+        >
+          {{ day.date() }}
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
+
+
